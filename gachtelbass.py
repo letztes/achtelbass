b81a643e6daa4a28bf21f191bbef3fe8e72ea466
Artur Spengler <letztes@gmail.com>
2010-09-02 01:41:05 +0200

    Added a 'How to use' section to the documentation.

--- gachtelbass.py
+++ gachtelbass.py
@@ -8,5 +8,21 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 
-class gachtelbass(self):
-   self.window1 = gtk.Window
+class gachtelbass(object):
+    def __init__(self):
+       self.window1 = gtk.Window(gtk.WINDOW_TOPLEVEL)
+       self.window1.set_title("achtelbass")
+       self.window1.connect("delete_event", self.delete_event)
+
+       self.window1.show()
+
+    def delete_event(self, widget, event, data=None):
+        gtk.main_quit()
+        return False
+
+def main():
+    gtk.main()
+    return 0
+
+gachtelbass()
+main()

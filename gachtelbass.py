8b3137c1263fd8e9b12cfc7d9299a9cefbe36d1e
Artur Spengler <letztes@gmail.com>
2010-09-03 02:15:56 +0200

    Experiments with Menubar.
    Menubar is for "File -> Open", "File -> Save as" or "File -> Quit" and so.
    This is necessary too, but what is most needed for now is rather
    gtk.ComboBox: A kind of Button that expands downwards when klicked, showing
    all the alternatives to chose. I hope at least that thats the right thing.

--- gachtelbass.py
+++ gachtelbass.py
@@ -2,7 +2,12 @@
 # example base.py
 # Tutorial: http://www.pygtk.org/pygtk2tutorial/
 # http://www.pygtk.org/pygtk2tutorial/sec-ToggleButtons.html
+# Drop-down-menu: http://www.pygtk.org/pygtk2tutorial/sec-ManualMenuExample.html
+# eher: http://www.pygtk.org/docs/pygtk/class-gtkcombobox.html
 
+# Was bisher implementiert ist, braucht man sowieso, nämlich die Menüs für
+# "Datei", "Speichern", "Beenden" usw. Dafür ist die menu_bar gut.
+# Aber für das normale Drop-Down-Auswählen braucht man eher die Combobox.
 
 import pygtk
 pygtk.require('2.0')
@@ -10,11 +15,33 @@ import gtk
 
 class gachtelbass(object):
     def __init__(self):
-       self.window1 = gtk.Window(gtk.WINDOW_TOPLEVEL)
-       self.window1.set_title("achtelbass")
-       self.window1.connect("delete_event", self.delete_event)
+        self.main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.main_window.set_title("achtelbass")
+        self.main_window.connect("delete_event", self.delete_event)
 
-       self.window1.show()
+        menu_tonic= gtk.Menu()
+        for current_tonic in ('C', 'G', 'D', 'A', 'E', 'H', 'Fis', 'Des', 'As', 'Es', 'B', 'F'):
+            menu_item = gtk.MenuItem(current_tonic)
+            menu_tonic.append(menu_item)
+            menu_item.connect("activate", self.select_tonic, current_tonic)
+            menu_item.show()
+
+        menu_item_tonic = gtk.MenuItem('Tonic')
+        menu_item_tonic.set_submenu(menu_tonic)
+        menu_item_tonic.show()
+
+        menu_bar = gtk.MenuBar()
+        menu_bar.append(menu_item_tonic)
+        menu_bar.show()
+
+        box = gtk.HBox()
+        self.main_window.add(box)
+        box.show()
+        box.pack_start(menu_bar)
+
+        self.main_window.show()
+    def select_tonic(self, widget, current_tonic):
+        print current_tonic
 
     def delete_event(self, widget, event, data=None):
         gtk.main_quit()
